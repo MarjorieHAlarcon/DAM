6b8e5794723a31f1c0ff577bf8547aee9dfb7596
Cetecom <marj.hernandeza@duocuc.cl>
2025-08-22 15:37:36 -0400

    Funciones

---
+++ NullSafety.kt
@@ -0,0 +1,24 @@
+fun main() {
+    println("Inicio de programa")
+
+    val a = 5
+    val b = 3
+
+    sumar(a,b)
+    sumar(9,6)
+    sumar(4,7)
+    sumar(93,1)
+}
+
+    /* Crear funcion  */
+
+fun sumar(a:Int, b:Int):Int{
+    print("Programa de sumas extremas - Voy a sumar $a con $b = ")
+    println(a+b)
+}
+
+fun restar(a:Int, b:Int):Int {
+    print("Programa de sumas extremas - Voy a restar $a con $b = ")
+    println(a - b)
+}
+
